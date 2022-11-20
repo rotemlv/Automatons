2916515b8dcfe53d2b7aee8523ee1519819cf029
rotemlv <rotemlv@gmail.com>
2022-11-20 17:34:12 +0200

    A basic implementation of some automatons (deterministic and non-deterministic finite automatons).
    9

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ from random import randint
 from DFA import random_word
 from NDFA import NonDeterministicAutomaton
 
+WORD_MIN_SIZE = 0
+WORD_MAX_SIZE = 12
+
+
 if __name__ == '__main__':
+    # defines an NDFA that accepts words starting with 'a' which have at least
+    # 1 'a' between every two 'b's
     NA = NonDeterministicAutomaton({'a', 'b'})
     NA.add_state(0)
     NA.add_state(1)
@@ -11,8 +17,9 @@ if __name__ == '__main__':
     # NA.traverse("abbaaa")
 
     random_words = set()
-    while len(random_words) < 10:
-        random_words.add(random_word({'a', 'b'}, randint(0, 20)))
+    while len(random_words) < 100:
+        random_words.add(random_word({'a', 'b'},
+                                     randint(WORD_MIN_SIZE, WORD_MAX_SIZE)))
 
     accepted_words = set()
     for w in random_words:
